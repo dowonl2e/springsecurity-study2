63551b32285ab24cb1457d7e6064c398eff19255
dowonl2e <dierwdod@gmail.com>
2022-02-26 19:03:44 +0900

    Refresh Token 관련 로그인 프로세스 수정 및 재발급 추가

--- src/main/java/com/study/member/service/AuthService.java
+++ src/main/java/com/study/member/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.study.member.service;
 
+import java.util.Optional;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -11,6 +13,7 @@ import com.study.exception.CustomException;
 import com.study.member.dto.MemberRequestDto;
 import com.study.member.dto.MemberResponseDto;
 import com.study.member.dto.TokenDto;
+import com.study.member.dto.TokenRequestDto;
 import com.study.member.entity.MemberRepository;
 import com.study.member.entity.RefreshToken;
 import com.study.member.entity.RefreshTokenRepository;
@@ -18,7 +21,9 @@ import com.study.provider.JwtTokenProvider;
 import com.study.status.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -46,24 +51,62 @@ public class AuthService {
 
 		//실제 검증 
 		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		System.out.println("인증 체크 : " + authentication.getName() + ", " + authentication.isAuthenticated());
 		if(authentication.isAuthenticated() == false)
-			throw new CustomException(ErrorCode.USER_MISMATCH_PWD);
-		
+			throw new CustomException(ErrorCode.USER_MISMATCH_PWD);    		//인증 정보를 기반으로 Token 생성
 		
 		//인증 정보를 기반으로 Token 생성
 		TokenDto tokenDto = jwtTokenProvider.generateTokenDto(authentication);
 		
-		//Token 저장
-		RefreshToken refreshToken = RefreshToken.builder()
-				.keyId(authentication.getName())
-				.keyValue(tokenDto.getRefreshToken())
-				.expiredDate(tokenDto.getRefreshExpiredDate())
-				.build();
+        Optional<RefreshToken> refreshTokenEntity = refreshTokenRepository.findByKeyId(authentication.getName());
+        //refreshToken 정보 여부 및 만료여부 체크
+        if(refreshTokenEntity.isEmpty()) {
+    		//Token 저장
+    		RefreshToken refreshToken = RefreshToken.builder()
+    				.keyId(authentication.getName())
+    				.keyValue(tokenDto.getRefreshToken())
+    				.expiredDate(tokenDto.getRefreshExpiredDate())
+    				.build();
+    		
+    		refreshTokenRepository.save(refreshToken);	
+        }
+        else {
+        	//Refresh Token 만료 체크
+            if (jwtTokenProvider.isValidToken(refreshTokenEntity.get().getKeyValue()) == false)
+        		refreshTokenEntity.get().update(tokenDto.getRefreshToken(), tokenDto.getRefreshExpiredDate());
+            else
+            	tokenDto
+            		.setRefreshToken(refreshTokenEntity.get().getKeyValue())
+            		.setRefreshExpiredDate(refreshTokenEntity.get().getExpiredDate());
+        }
 		
-		refreshTokenRepository.save(refreshToken);
 		
 		return tokenDto;
 	}
 	
+	@Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+        if (jwtTokenProvider.isValidToken(tokenRequestDto.getRefreshToken()) == false) {
+            throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
+        }
+
+        // Access Token 에서 Member ID 가져오기
+        Authentication authentication = jwtTokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        RefreshToken refreshToken = refreshTokenRepository.findByKeyId(authentication.getName())
+        		.orElseThrow(() -> new CustomException(ErrorCode.USER_LOGOUT));
+
+        // Refresh Token 검사
+        if (!refreshToken.getKeyValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new CustomException(ErrorCode.MISMATCH_REFRESH_TOKEN);
+        }
+
+        // 새로운 토큰 생성
+        TokenDto tokenDto = jwtTokenProvider.generateTokenDto(authentication);
+
+        // 6. RefreshToken 정보 업데이트
+        refreshToken.update(tokenDto.getRefreshToken(), tokenDto.getRefreshExpiredDate());
+
+        return tokenDto;
+    }
+
 }
